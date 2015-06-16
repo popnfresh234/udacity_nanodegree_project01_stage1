e1aaa2a386d83ba3bdb4a6f6f116329104c02356
Alex <ajhollid@gmail.com>
2015-06-16 23:15:35 +0800

    ArrayList in mainFragment now cleared every time new search made

--- app/src/main/java/com/dmtaiwan/alexander/spotifystreamer/MainFragment.java
+++ app/src/main/java/com/dmtaiwan/alexander/spotifystreamer/MainFragment.java
@@ -61,99 +61,99 @@ public class MainFragment extends android.support.v4.app.Fragment {
             }
         });
 
+        mArtistArray = new ArrayList<ParcelableArtist>();
+        mArtistField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    mArtistArray.clear();
+                    Utils.hideKeyboard(getActivity(), getActivity().getCurrentFocus());
+                    String query = mArtistField.getText().toString();
+                    SpotifyApi api = new SpotifyApi();
+                    final SpotifyService spotify = api.getService();
+                    spotify.searchArtists(query, new Callback<ArtistsPager>() {
+
+                        @Override
+                        public void success(ArtistsPager artistsPager, Response response) {
+                            List<Artist> artists = artistsPager.artists.items;
+                            if (artists.size() > 0) {
+                                for (int i = 0; i < artists.size(); i++) {
+                                    Artist artist = artists.get(i);
+                                    String artistName = artist.name;
+                                    String imageUrl = null;
+                                    String artistId = artist.id;
+                                    List<Image> images = artist.images;
+                                    if (images.size() > 0) {
+                                        for (int j = 0; j < images.size(); j++) {
+                                            Image image = images.get(j);
+                                            int height = image.height;
+                                            if (height <= 200 && height <= 220) {
+                                                imageUrl = image.url;
+                                            }
+                                        }
+                                    }
+                                    ParcelableArtist
+                                            parcelableArtist = new ParcelableArtist(artistName, imageUrl, artistId);
+                                    mArtistArray.add(parcelableArtist);
+                                }
+
+                                if (mAdapter == null) {
+                                    mAdapter = new ArtistsAdapter(getActivity(), R.layout.list_item_artist, mArtistArray);
+
+                                } else {
+                                    //If there's already a top tracks fragment shown, remove it
+                                    Fragment fragment = getActivity().getSupportFragmentManager().findFragmentByTag("toptracks");
+                                    if (fragment != null) {
+                                        getActivity().getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+                                    }
+
+                                    //update adapter with new data
+                                    getActivity().runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            mAdapter.notifyDataSetChanged();
+                                        }
+                                    });
+
+                                }
+
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mListView.setAdapter(mAdapter);
+                                    }
+                                });
+
+                            } else {
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(getActivity(), getString(R.string.toast_no_artist_error), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+
+                            }
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            error.printStackTrace();
+                        }
+                    });
+                    return true;
+                }
+                return false;
+            }
+        });
+
         if (savedInstanceState != null && savedInstanceState.getParcelableArrayList(OUTSTATE_ARTIST_ARRAY).size()>0) {
             mArtistArray = savedInstanceState.getParcelableArrayList(OUTSTATE_ARTIST_ARRAY);
             mAdapter = new ArtistsAdapter(getActivity(), R.layout.list_item_artist, mArtistArray);
             mAdapter.notifyDataSetChanged();
             //hide soft keyboard on rotate
             getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
-        } else {
-            mArtistArray = new ArrayList<ParcelableArtist>();
-            mArtistField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                @Override
-                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                    if (actionId == EditorInfo.IME_ACTION_DONE) {
-                        mArtistArray.clear();
-                        Utils.hideKeyboard(getActivity(), getActivity().getCurrentFocus());
-                        String query = mArtistField.getText().toString();
-
-                        SpotifyApi api = new SpotifyApi();
-                        final SpotifyService spotify = api.getService();
-                        spotify.searchArtists(query, new Callback<ArtistsPager>() {
-
-                            @Override
-                            public void success(ArtistsPager artistsPager, Response response) {
-                                List<Artist> artists = artistsPager.artists.items;
-                                if (artists.size() > 0) {
-                                    for (int i = 0; i < artists.size(); i++) {
-                                        Artist artist = artists.get(i);
-                                        String artistName = artist.name;
-                                        String imageUrl = null;
-                                        String artistId = artist.id;
-                                        List<Image> images = artist.images;
-                                        if (images.size() > 0) {
-                                            for (int j = 0; j < images.size(); j++) {
-                                                Image image = images.get(j);
-                                                int height = image.height;
-                                                if (height <= 200 && height <= 220) {
-                                                    imageUrl = image.url;
-                                                }
-                                            }
-                                        }
-                                        ParcelableArtist
-                                                parcelableArtist = new ParcelableArtist(artistName, imageUrl, artistId);
-                                        mArtistArray.add(parcelableArtist);
-                                    }
-
-                                    if (mAdapter == null) {
-                                        mAdapter = new ArtistsAdapter(getActivity(), R.layout.list_item_artist, mArtistArray);
-
-                                    }else {
-                                        //If there's already a top tracks fragment shown, remove it
-                                        Fragment fragment = getActivity().getSupportFragmentManager().findFragmentByTag("toptracks");
-                                        if(fragment!= null) {
-                                            getActivity().getSupportFragmentManager().beginTransaction().remove(fragment).commit();
-                                        }
-
-                                        //update adapter with new data
-                                        getActivity().runOnUiThread(new Runnable() {
-                                            @Override
-                                            public void run() {
-                                                mAdapter.notifyDataSetChanged();
-                                            }
-                                        });
-
-                                    }
-
-                                    getActivity().runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            mListView.setAdapter(mAdapter);
-                                        }
-                                    });
-
-                                } else {
-                                    getActivity().runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            Toast.makeText(getActivity(), getString(R.string.toast_no_artist_error), Toast.LENGTH_LONG).show();
-                                        }
-                                    });
-
-                                }
-                            }
-
-                            @Override
-                            public void failure(RetrofitError error) {
-                                error.printStackTrace();
-                            }
-                        });
-                        return true;
-                    }
-                    return false;
-                }
-            });
         }
+
         return rootView;
     }
 
